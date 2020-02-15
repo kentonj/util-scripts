d7c7af87a81bfbb3f0f38aefb4c0b1a7e269dd38
Kenton <kzart94@gmail.com>
2020-02-15 13:37:25 -0700

    Delete updateKernel.sh

--- updateKernel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Current kernel: $(uname -r)"
-echo "updating the kernel..."
-sudo apt-get install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-18.04
-echo "
-for changes to go into effect, you'll need to reboot. After rebooting, run ./pimpMyUbuntu.sh
-"
-
